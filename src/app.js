56075055d477461e37d89cb5b75970821f7e17b9
Vasilios Magriplis <etameson@gmail.com>
2019-10-19 16:48:18 +1100

    Minor updates

--- src/app.js
+++ src/app.js
@@ -3,7 +3,11 @@ import cors from 'cors';
 import 'dotenv/config';
 
 const app = express();
+
+// Apply middlware for CORS and JSON endpoing
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
